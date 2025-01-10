7baf84485d284d12e055691287e44a48720c219b
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-01-10 17:26:01 -0300

    feat: resolução do teste 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'desafio-target-dev-frontend';
+  titleProject = 'Desafio Target - Dev Front-end'
+
+  SUM = 0
+
+  teste1(): number {
+    const INDEX = 13
+    let K = 0
+
+    while (this.SUM < INDEX) {
+      K = K + 1
+      this.SUM = this.SUM + K
+      console.info(this.SUM)
+    }
+
+    return this.SUM
+  }
+
+
+  ngOnInit() {
+    this.teste1()
+  }
+
 }
