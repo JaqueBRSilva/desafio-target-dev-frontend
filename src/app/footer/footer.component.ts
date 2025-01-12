9d39fcf40c0cf168f99da919fdad95f8e7178893
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-01-12 17:33:19 -0300

    feat: criado componente 'footer'

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  username = 'JaqueBRSilva'
+  userLink = 'https://linktr.ee/jaquebrsilva'
+  createdYear = '2025'
+}
