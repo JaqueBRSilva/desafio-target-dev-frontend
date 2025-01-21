ea94bb5f20f83129b7b5d1e320600b730473dff6
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-01-21 18:34:27 -0300

    feat: add desafios como componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Desafio1Component } from './desafio-1/desafio-1.component';
+import { Desafio2Component } from './desafio-2/desafio-2.component';
+import { Desafio3Component } from './desafio-3/desafio-3.component';
+import { Desafio4Component } from './desafio-4/desafio-4.component';
+import { Desafio5Component } from './desafio-5/desafio-5.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [Desafio1Component, Desafio2Component, Desafio3Component, Desafio4Component,
+    Desafio5Component, RouterOutlet, HeaderComponent, FooterComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // TESTE 1
-  sumCount = 0
-
-  teste1(): number {
-    const INDEX = 13
-    let K = 0
-
-    while (K < INDEX) {
-      K = K + 1
-      this.sumCount = this.sumCount + K
-    }
-
-    return this.sumCount
-  }
-
-
-  ngOnInit() {
-    this.teste1()
-  }
 
 }
